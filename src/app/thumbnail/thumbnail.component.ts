d481a9642cd59432a21425bf63847dba7565b761
splacentino <39631@heb.be>
2016-05-14 02:25:04 +0100

    Insert play icon from materializecss

--- src/app/thumbnail/thumbnail.component.ts
+++ src/app/thumbnail/thumbnail.component.ts
@@ -5,11 +5,22 @@ import { Component, Input, OnInit, HostListener, HostBinding } from '@angular/co
   template: `
       <div>
         <h5>{{title}}</h5>
-        <img 
-            [src]="url"
-            [style.opacity]="hover ? 0.4 : 1"
-            class="responsive-img"
-        />
+        <div [style.position]="relative">
+            <img  
+                [src]="url"
+                [style.opacity]="hover ? 0.4 : 1"
+                [style.position]="relative"
+                [style.z-index]="0"
+                class="responsive-img"
+            />
+                
+            <i 
+                [hidden]="!hover" 
+                [style.z-index]="1"
+                class="material-icons medium icon-demo">
+                play_arrow
+            </i>
+        </div>
       </div>
     `
 })
@@ -25,5 +36,9 @@ export class ThumbnailComponent {
     
     @HostListener('mouseleave') onMouseLeave() {
         this.hover = false;
+    }
+    
+    @HostListener('mouseClick') onMouseClick() {
+        // TODO
     }
 }
