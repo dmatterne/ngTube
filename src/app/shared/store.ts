5f196b8e26ff490efef21f1752d621aa6beca3f1
Florian Knop <florian_knop@hotmail.com>
2016-05-14 09:22:43 +0200

    sidenav

--- src/app/shared/store.ts
+++ src/app/shared/store.ts
@@ -1,8 +1,8 @@
 export interface NgTubeStore {
-    history: any[],
-    minimize:   boolean,
-    play:       string,
-    playlist:   any[],
-    repeat:     string,
-    search:   string[]
+    history: any[];
+    minimize:   boolean;
+    play:       string;
+    playlist:   any[];
+    repeat:     string;
+    search:   string;
 }
