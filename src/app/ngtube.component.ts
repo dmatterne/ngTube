97607107ff0e80b1d2099b3f32f0c83dfadd7f15
Florian Knop <florian_knop@hotmail.com>
2016-05-14 03:08:13 +0200

    Add layout components

--- src/app/ngtube.component.ts
+++ src/app/ngtube.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from './navbar';
+import { SidenavComponent } from './sidenav';
 
 @Component({
   moduleId: module.id,
   selector: 'ngtube-app',
   templateUrl: 'ngtube.component.html',
-  styleUrls: ['ngtube.component.css']
+  styleUrls: ['ngtube.component.css'],
+  directives: [NavbarComponent, SidenavComponent]
 })
 export class NgtubeAppComponent {
-  title = 'ngtube works!';
+  
 }
--- src/system-config.ts
+++ src/system-config.ts
@@ -29,6 +29,8 @@ const barrels: string[] = [
   // App specific barrels.
   'app',
   'app/shared',
+  'app/navbar',
+  'app/sidenav',
   /** @cli-barrel */
 ];
 
