899c579d093536d0968931d7a241aca8a0cff5fd
Florian Knop <florian_knop@hotmail.com>
2016-05-14 06:14:23 +0200

    Add youtube search service

--- src/app/ngtube.component.ts
+++ src/app/ngtube.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, provide } from '@angular/core';
+import { HTTP_PROVIDERS } from '@angular/http';
 import { NavbarComponent } from './navbar';
 import { SidenavComponent } from './sidenav';
 import { ThumbnailListComponent } from './thumbnail-list';
 import { Video } from './shared';
 import { YoutubePlayerComponent } from './youtube-player';
 import { YoutubePlayerService } from './youtube-player.service';
+import { YoutubeSearchService } from './youtube-search.service';
+import { config, APP_CONFIG } from './shared';
 
 @Component({
   moduleId: module.id,
   selector: 'ngtube-app',
   templateUrl: 'ngtube.component.html',
+  providers: [
+      provide(APP_CONFIG, { useValue: config }),
+      HTTP_PROVIDERS,
+      YoutubeSearchService
+  ],
   styleUrls: ['ngtube.component.css'],
   directives: [NavbarComponent, SidenavComponent, YoutubePlayerComponent, ThumbnailListComponent]
 })
--- src/app/youtube-search.service.ts
+++ src/app/youtube-search.service.ts
@@ -1,15 +1,64 @@
-import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import { Injectable, Inject } from '@angular/core';
+import { Http, URLSearchParams } from '@angular/http';
+import { APP_CONFIG, config, AppConfiguration } from './shared';
+
+export enum YoutubeVideoDuration {
+    ANY,
+    SHORT,
+    MEDIUM,
+    LONG
+}
+
+export interface YoutubeSearchOptions {
+    pageToken?: string;
+    videoOnly?: boolean;
+    duration?: YoutubeVideoDuration | string;
+}
 
 @Injectable()
 export class YoutubeSearchService {
 
-  constructor (private http: Http) {}
+  constructor (@Inject(APP_CONFIG) private config: AppConfiguration, private http: Http) {}
 
   
-  findAll () {
+  findAll (query: string, options: YoutubeSearchOptions = {}) {
       
-    //   this.http.get()
+      const search = new URLSearchParams();
+      search.append('part', 'snippet');
+      search.append('key', this.config.apiKey);
+      search.append('type', options.videoOnly ? 'true' : 'false');
+      search.append('q', query);
+      
+      if (options.duration) {
+          search.append('videoDuration', this.getDuration(options.duration));
+      }
+      
+      if (options.pageToken) {
+        search.append('pageToken', options.pageToken);
+      }
+      
+      const url = `${this.config.apiEndpoint}/search`;
+      
+      return this.http.get(url, { search });
+  }
+  
+  private getDuration (duration: YoutubeVideoDuration | string) {
+      
+      if (typeof duration === 'string') {
+          return duration;
+      }
+      
+      switch (duration) {
+          
+        case YoutubeVideoDuration.ANY: 
+            return 'any';
+        case YoutubeVideoDuration.SHORT:
+            return 'short';
+        case YoutubeVideoDuration.MEDIUM:
+            return 'medium';
+        case YoutubeVideoDuration.LONG:
+            return 'long';
+      }
   }
 
 }
