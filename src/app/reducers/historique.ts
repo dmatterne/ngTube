82df094c5bda6825ada13cc19ab68a97d46cbff8
splacentino <39631@heb.be>
2016-05-14 05:33:54 +0100

    Add historique reducer

---
+++ src/app/reducers/historique.ts
@@ -0,0 +1,14 @@
+ import {Reducer, Action} from '@ngrx/store';
+
+export const historique: Reducer<any[]> = (state = [], action) => {
+    switch (action.type) {
+        case 'ADD_PLAYLIST':
+            return [action.payload.video, ...state];
+        case 'REMOVE_PLAYLIST':
+            return state.filter(video => video.id != action.payload.id);
+        case 'PURGE_PLAYLIST':
+            return [];
+        default:
+            return state;
+    }
+}
--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -2,3 +2,4 @@ export { minimize } from './minimize';
 export { repeat } from './repeat';
 export { play } from './play';
 export { playlist } from './playlist';
+export { historique } from './historique';
