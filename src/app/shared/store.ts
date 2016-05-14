a34ff07f7ed809a107453fc30da90b89d2679b46
Florian Knop <florian_knop@hotmail.com>
2016-05-14 12:03:34 +0200

    Add locale storage and fix import

--- src/app/shared/store.ts
+++ src/app/shared/store.ts
@@ -1,9 +1,11 @@
+import { SizeState, PlayState, RepeatState } from '../reducers';
+
 export interface NgTubeStore {
     history: any[];
-    minimize:   boolean;
-    play:       string;
-    playlist:   any[];
-    repeat:     string;
-    search:   string;
+    minimize: SizeState;
+    play: PlayState;
+    playlist: any[];
+    repeat: RepeatState;
+    search: string;
     currentVideo: string;
 }
