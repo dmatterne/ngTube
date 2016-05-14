cb0c2a181305209dbdfca23b12947d8169f1790a
splacentino <39631@heb.be>
2016-05-14 02:26:11 +0100

    Merge from master

--- src/app/ngtube.component.ts
+++ src/app/ngtube.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
-
+import { ThumbnailComponent } from './thumbnail';
 @Component({
   moduleId: module.id,
   selector: 'ngtube-app',
-  templateUrl: 'ngtube.component.html',
-  styleUrls: ['ngtube.component.css']
+  template: `<h1>
+    {{title}}
+    </h1>
+    <thumbnail 
+        title="Kaaris - Le bruit de mon âme" 
+        url="https://i.ytimg.com/vi/LBr3HjsUZBg/default.jpg">
+    </thumbnail>
+    `,
+  styleUrls: ['ngtube.component.css'],
+  directives: [ThumbnailComponent]
 })
 export class NgtubeAppComponent {
   title = 'ngtube works!';
