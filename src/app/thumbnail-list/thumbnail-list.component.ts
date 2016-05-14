17c79992a88fcbf1a79bdf0c3b7a60e7f8cd12c8
splacentino <39631@heb.be>
2016-05-14 17:13:40 +0100

    Select item if exists in playlist and is playing

--- src/app/thumbnail-list/thumbnail-list.component.ts
+++ src/app/thumbnail-list/thumbnail-list.component.ts
@@ -107,5 +107,6 @@ export class ThumbnailListComponent implements OnDestroy {
     
     onTitleClick (video: Video) {
         this.store.dispatch({ type: 'ADD_TO_PLAYLIST', payload: { video: video } });
+        this.store.dispatch({ type: 'SELECT_ITEM', payload: { video: video.id } });
     }
 }
