b1fabc34f285279297521a736ecea4a017b84920
splacentino <39631@heb.be>
2016-05-14 05:14:12 +0100

    Add minimize reducer

--- src/app/reducers/minimize.ts
+++ src/app/reducers/minimize.ts
@@ -1,8 +1,9 @@
 import {Reducer, Action} from '@ngrx/store';
 
-export const minimize: Reducer<boolean> = (state = false, action) => {
+export const minimize: Reducer<boolean> = (state = false,  action) => {
     switch (action.type) {
-
+        case "SET_MINIMIZE":
+            return action.payload.minimize;
         default:
             return state;
     }
