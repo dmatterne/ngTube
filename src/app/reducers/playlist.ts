5acdb198531e1f10ff80e602891072e6c8f309f4
splacentino <39631@heb.be>
2016-05-14 12:00:08 +0100

    Add select video from playlist reducer

--- src/app/reducers/playlist.ts
+++ src/app/reducers/playlist.ts
@@ -2,6 +2,8 @@
  
 export const playlist: Reducer<any[]> = (state = [], action) => {
     switch (action.type) {
+        case 'SELECT_ITEM':
+            return state.map(video => selectItem(video, action.payload.id));
         case 'ADD_PLAYLIST':
             return [action.payload.video, ...state];
         case 'REMOVE_PLAYLIST':
@@ -10,5 +12,13 @@ export const playlist: Reducer<any[]> = (state = [], action) => {
             return [];
         default:
             return state;
+    }
+}
+
+const selectItem = (state: any = {}, id) => {
+    if (state.id === id) {
+        return Object.assign({}, state, { selected: true });
+    } else {
+        return state;
     }
 }
