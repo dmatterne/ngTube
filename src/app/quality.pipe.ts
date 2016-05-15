c5ac4a3f174b2b06e0cf2c1dc2c879b2b56f4b0b
Florian Knop <florian_knop@hotmail.com>
2016-05-15 20:31:07 +0200

    Fix truncate

--- src/app/quality.pipe.ts
+++ src/app/quality.pipe.ts
@@ -6,7 +6,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class Quality implements PipeTransform {
 
   transform(value: any, args?: any): any {
-    console.log('transform value: ' + value);
+    
     switch (value) {
       case 'auto':
         return 'Default';
