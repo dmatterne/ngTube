01da66aef2463b1722046c3d5a76e86b0cf7accf
Florian Knop <florian_knop@hotmail.com>
2016-05-15 15:11:30 +0200

    Style description

---
+++ src/app/newlines.pipe.ts
@@ -0,0 +1,17 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'newlines'
+})
+export class NewlinesPipe implements PipeTransform {
+
+  transform(input: any, args?: any): any {
+    
+    if (typeof input !== 'string') {
+        return input;
+    }
+    
+    return input.replace(/(?:\r\n|\r|\n)/g, '<br />');
+  }
+
+}
